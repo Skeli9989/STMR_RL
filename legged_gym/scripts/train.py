b7387c9bb1a23c165ef3b67f71d263baf7b4c9f7
terry <taerimyoon@korea.ac.kr>
2024-01-26 10:16:38 +0900

    add seed name to log dir

--- legged_gym/scripts/train.py
+++ legged_gym/scripts/train.py
@@ -38,7 +38,7 @@ from legged_gym.utils import get_args, task_registry
 import torch
 
 def train(args):
-    register_tasks(args.task)
+    register_tasks(args.task, args.seed)
     env, env_cfg = task_registry.make_env(name=args.task, args=args)
     ppo_runner, train_cfg = task_registry.make_alg_runner(env=env, name=args.task, args=args)
     ppo_runner.learn(num_learning_iterations=train_cfg.runner.max_iterations, init_at_random_ep_len=True)
