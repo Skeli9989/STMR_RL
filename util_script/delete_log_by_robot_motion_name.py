31b6d189c19ac6a0e5610595fd730e130ba22526
terry <taerimyoon@korea.ac.kr>
2024-02-17 13:42:27 +0900

    wrong albase trot, pace motion

---
+++ util_script/delete_log_by_robot_motion_name.py
@@ -0,0 +1,29 @@
+# %%
+from pathlib import Path
+import glob
+import shutil
+from legged_gym import LEGGED_GYM_ROOT_DIR
+import os
+
+ROBOT_NAME_TO_DELETE = "albase"
+MOTION_NAME_TO_DELETE = ["trot0", "trot1", "pace0", "pace1"]
+logs_names = ["logs50", "logs63", "logs64", "logs65", 'logs']
+
+for logs_name in logs_names:
+    logs_dir_path = Path(f"{LEGGED_GYM_ROOT_DIR}/{logs_name}")
+    if not logs_dir_path.exists(): continue
+
+    for RAND_NORAND in ["RAND", "STMR"]:
+        RAND_NORAND_path = logs_dir_path/RAND_NORAND
+        if not RAND_NORAND_path.exists(): continue
+
+        for MOTION_NAME in MOTION_NAME_TO_DELETE:
+            motion_path = RAND_NORAND_path/MOTION_NAME
+            if not motion_path.exists(): continue
+            for robot_path in motion_path.iterdir():
+                if robot_path.name == ROBOT_NAME_TO_DELETE:
+                    print('deteleting', robot_path)
+                    shutil.rmtree(robot_path)
+
+from util_script.checklist import main
+main()
