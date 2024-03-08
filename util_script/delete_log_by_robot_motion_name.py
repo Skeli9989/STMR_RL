adf741457fffca431eb40b6aafc7421c7c5ddb34
terry <taerimyoon@korea.ac.kr>
2024-03-08 20:46:47 +0900

    delete log script

--- util_script/delete_log_by_robot_motion_name.py
+++ util_script/delete_log_by_robot_motion_name.py
@@ -5,8 +5,9 @@ import shutil
 from legged_gym import LEGGED_GYM_ROOT_DIR
 import os
 
-ROBOT_NAME_TO_DELETE = "go1base"
+ROBOT_NAME_TO_DELETE = ["go1base"]
 MOTION_NAME_TO_DELETE = ["trot0", "pace0", "trot1", "pace1"]
+MR_NAME_TO_DELETE = ["STMR"]
 logs_names = ["logs50", "logs63", "logs64", "logs65", 'logs']
 
 for logs_name in logs_names:
@@ -21,9 +22,12 @@ for logs_name in logs_names:
             motion_path = RAND_NORAND_path/MOTION_NAME
             if not motion_path.exists(): continue
             for robot_path in motion_path.iterdir():
-                if robot_path.name == ROBOT_NAME_TO_DELETE:
-                    print('deteleting', robot_path)
-                    shutil.rmtree(robot_path)
+                if robot_path.name in ROBOT_NAME_TO_DELETE:
+                    for mr_path in robot_path.iterdir():
+                        if mr_path.name in MR_NAME_TO_DELETE:
+                            print('deteleting', mr_path)
+                            # shutil.rmtree(robot_path)
+
 
 from util_script.checklist import main
 main()
