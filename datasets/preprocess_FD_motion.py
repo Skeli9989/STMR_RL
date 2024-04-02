1defe1a4c0d74860e7066fd4b90adb11990d5d53
terry <taerimyoon@korea.ac.kr>
2024-04-02 12:54:59 +0900

    videwalk1 policy train

--- datasets/preprocess_FD_motion.py
+++ datasets/preprocess_FD_motion.py
@@ -27,6 +27,7 @@ ROBOT = ROBOT.lower()
 MOTIONS = ['go1trot', 'hopturn', 'pace0', 'pace1', 'sidesteps', 'trot0', 'trot1']
 MOTIONS = ['hopturn', 'pace0', 'pace1', 'sidesteps', 'trot0', 'trot1']
 MOTIONS = ['pace0', 'pace1', 'trot0', 'trot1']
+MOTIONS = ['videowalk0', 'videowalk1']
 # MOTIONS = ['pace1', 'trot1']
 
 for MOTION in MOTIONS:
